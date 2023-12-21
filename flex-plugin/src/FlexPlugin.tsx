8a15879d0a69d4ebdb8b696b53e3b3b5091ad844
Bruno Kilian <bkilian@twilio.com>
2023-12-21 10:23:15 +0100

    flex-plugin: remove panel2 when loading from Chrome Extension

--- flex-plugin/src/FlexPlugin.tsx
+++ flex-plugin/src/FlexPlugin.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import * as Flex from '@twilio/flex-ui';
 import { FlexPlugin } from '@twilio/flex-plugin';
 
-import CustomTaskList from './components/CustomTaskList/CustomTaskList';
-
 const PLUGIN_NAME = 'FlexPlugin';
 
 export default class FlexPluginChromeExtension extends FlexPlugin {
@@ -11,14 +9,29 @@ export default class FlexPluginChromeExtension extends FlexPlugin {
     super(PLUGIN_NAME);
   }
 
-  /**
-   * This code is run when your plugin is being started
-   * Use this to modify any UI components or attach to the actions framework
-   *
-   * @param flex { typeof Flex }
-   */
   async init(flex: typeof Flex, manager: Flex.Manager): Promise<void> {
-    const options: Flex.ContentFragmentProps = { sortOrder: -1 };
-    flex.AgentDesktopView.Panel1.Content.add(<CustomTaskList key="FlexPlugin-component" />, options);
+    removePanel2IfRunningFromChromeExtension(flex, manager);    
+  }
+}
+
+function removePanel2IfRunningFromChromeExtension(flex: typeof Flex, manager: Flex.Manager) {
+  try {    
+    const baseUrl = window.location.ancestorOrigins[0];
+    if (baseUrl.startsWith('chrome-extension://')) {
+
+      // Remove Panel2
+      manager.updateConfig({
+        componentProps: {
+          AgentDesktopView: {
+            showPanel2: false
+          }
+        }
+      })      
+
+      // Remove Menu
+      flex.MainHeader.Content.remove('sidenav-button')
+    }
+  } catch(e) {
+    console.error('removePanel2IfRunningFromChromeExtension catch: ', e);
   }
 }
